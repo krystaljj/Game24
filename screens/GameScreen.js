06ac88cd5798b04e4c55dc056b854bcebcb2b228
matttattoli@gmail.com <matttattoli@gmail.com>
2020-03-27 22:58:42 -0400

    clean up styles

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -12,17 +12,11 @@ export default class GameScreen extends React.Component {
     };
   }
 
-  incrementCount () {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  }
-
   render() {
     return (
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <TouchableOpacity style={styles.playContainer} onPress={() => this.props.navigation.navigate('HomeScreen')}>
-          <Text style={styles.playButton}>1</Text>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('HomeScreen')}>
+          <Text>Main Menu</Text>
         </TouchableOpacity>
       </ScrollView>
     );
@@ -36,24 +30,5 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
   },
 });
